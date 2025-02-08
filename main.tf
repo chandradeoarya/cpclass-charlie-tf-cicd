c64df011954b57410a6975e2aae8b27040f7fe7c
Chandradeo Arya <chandradeoaryaarya@gmail.com>
2025-02-08 21:39:18 +0530

    adding

---
+++ main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+  region  = "us-east-1"
+}
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+}
+
+resource "aws_s3_bucket" "tf_s3_cicd" {
+  bucket = "chandra-cpclass-charlie-tf-cicd-v1"  # Replace with your desired bucket name
+  tags = {
+    "name" = "tfcicd-bucket",
+    "team" = "devops"
+    "role" = "sre"
+    "company" = "cp"
+    "text" = "checking pull request"
+  }
+}
+output "bucket_id" {
+  description = "Bucket ID"
+  value = aws_s3_bucket.tf_s3_cicd.id
+}
